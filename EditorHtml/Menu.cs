788864262b9ce5153b0a5a008e55d620daafa9e6
Gustavo <gust.bersan@gmail.com>
2022-04-04 23:34:54 -0300

    Encapsulamento
    Utilizando dois métodos (Draw e DrawLines), foi encapsulado o código que iria se repetir em alguns pontos do desenho da interface

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -12,17 +12,27 @@ namespace EditorHtml
 
             DrawScreen();
         }
-        public static void Draw()
+        public static void Draw(string symbol, string symbolValue, int value)
         {
+            Console.Write(symbol);
+            for (int columns = 0; columns <= value; columns++)
+                Console.Write(symbolValue);
+            Console.Write(symbol);
+            Console.Write("\n");
+
+        }
+        public static void DrawLines()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+                Draw("|", " ", 30);
 
         }
 
         public static void DrawScreen()
         {
-            Console.Write("+");
-            for (int columns = 0; columns <= 30; columns++)
-                Console.Write("-");
-            Console.Write("+");
+            Draw("+", "-", 30);
+            DrawLines();
+            Draw("+", "-", 30);
         }
     }
 }
